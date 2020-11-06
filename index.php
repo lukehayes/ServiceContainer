0a8030e313eb323ee354d7a9abca5c76c0079be0
lukehayes <lukepurplehayes@gmail.com>
2020-11-06 17:48:16 +0000

    Registered FormService into ServiceContainer

--- index.php
+++ index.php
@@ -1,20 +1,21 @@
 <?php
-
 require "vendor/autoload.php";
 
 use ServiceContainer\ServiceContainer;
-use ServiceContainer\ServiceProvider;
 use Services\TwigService;
 use Services\DoctrineService;
+use Services\FormService;
 
 $c = new ServiceContainer();
-
 $c->register('twig', TwigService::class);
 $c->register('doctrine', DoctrineService::class);
-
-$db = $c->get('doctrine')->fetchAll("select * from posts");
-
-dump($db);
+$c->register('form', FormService::class);
 
 
+$form = $c->get('form');
 
+$form->addText('name', 'Name');
+$form->addPassword('password', 'Password:');
+$form->addSubmit('send', 'Sign up');
+
+$form->render();
