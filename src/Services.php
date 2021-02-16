ed3c8d1426b3d6d589c922a21ac6714ae866bf3c
lukehayes <lukepurplehayes@gmail.com>
2021-02-16 17:43:10 +0000

    Added src/Services.php

---
+++ src/Services.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * All default services are defined in this file
+ */
+
+return [
+    'twig'     => TwigService::class,
+    'doctrine' => DoctrineService::class,
+    'form'     => FormService::class
+];
+
